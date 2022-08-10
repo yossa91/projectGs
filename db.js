de643508bb4171df548a63d441a39431b7645a88
yossa91 <y0ssa@naver.com>
2022-08-10 15:05:16 +0900

    0810

--- db.js
+++ db.js
@@ -9,6 +9,26 @@ const connection = mysql.createConnection({
     dateStrings : 'date'
 })
 
+//검색
+exports.search_process = function(seachbox, searchText, callback){
+    if(seachbox == 'noticeSub'){
+        connection.query(`SELECT * FROM notice WHERE description LIKE "%${searchText}%" ORDER BY no DESC;`,(err, rows, fields) => {
+            if(err) throw err;
+            callback(rows);
+        });
+    }else if(seachbox == 'noticeTitle'){
+        connection.query(`SELECT * FROM notice WHERE title LIKE "%${searchText}%" ORDER BY no DESC;`,(err, rows, fields) => {
+            if(err) throw err;
+            callback(rows);
+        });
+    }else if(seachbox == '' || seachbox == undefined || seachbox == null){
+        connection.query(`SELECT * FROM notice WHERE title || description LIKE "%${searchText}%" ORDER BY no DESC;`,(err, rows, fields) => {
+            if(err) throw err;
+            callback(rows);
+        }); 
+    }
+}
+
 //페이지 리스트
 exports.getAllList = function(callback){
     connection.query('SELECT * FROM notice ORDER BY no DESC;',(err, rows, fields) => {
@@ -16,13 +36,45 @@ exports.getAllList = function(callback){
         callback(rows);
     });
 }
- //리딩
- exports.page = function(no, callback){
-    connection.query(`SELECT * FROM notice WHERE no=?`,[no],(err, result) => {
-        if(err) {
-            throw err;}
-        callback(result);
+//새글작성
+exports.create = function(title, description, callback){
+    connection.query(`INSERT INTO notice(title,description,day,hit) VALUES('${title}','${description}',now(),0)`, (err, result) => {
+        if(err) throw err;
+        callback();
     });
  }
+ //리딩
+ exports.page = function(no, callback){
+    connection.query(`UPDATE notice set hit=hit+1 WHERE no = ${no}`,(err, result) => {
+        connection.query(`SELECT * FROM notice WHERE no=?`,[no],(err, result) => {
+            if(err) {
+                throw err;}
+            callback(result);
+        });
+    });
+ }
+ //수정
+ exports.update = function(no, callback){
+    connection.query(`SELECT * FROM notice WHERE no=?`,[no],(err, row, fields) => {
+        if(err) {
+            throw err;}
+        callback(row);
+    });
+ }
+ //수정후 저장(sql update)
+ exports.update_process = function(no, title, description, callback){
+    connection.query(`UPDATE notice set title = "${title}", description = "${description}" WHERE no = ${no}`, (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+ }
+ //글삭제
+ exports.remove = function(no, callback){
+    connection.query(`DELETE FROM notice WHERE no = ${no}`,(err) => {
+        if(err) throw err;
+        callback();
+    });
+ };
+
 
 
--- routes/route.js
+++ routes/route.js
@@ -35,21 +35,100 @@ router.get('/duty', (req,res) => {
 });
 
 //고객센터 
+//공지사항 검색
+router.post('/search',(req, res) =>{
+  let errs = validationResult(req);
+  let param = JSON.parse(JSON.stringify(req.body));
+  var seachbox = param['searchOp'];
+  let searchText = param['searchTx'];
+    if(errs['errors'].length > 0){
+    db.update_process(no, (row) => {
+      res.render('service_notice.ejs', {row : row[0], errs : errs['errors']} );
+    });
+  }else{
+    db.search_process(seachbox, searchText, (rows) => {
+      res.render('service_notice.ejs', { rows : rows });
+    });
+  }
+});
 //공지사항 list page
 router.get('/notice', (req,res) => {
   db.getAllList((rows) => {
-      res.render('service_notice', { rows : rows });
+      res.render('service_notice.ejs', { rows : rows });
     }
   );
 });
+//공지사항 create page
+router.get('/create', (req,res) => {
+  res.render('service_notice_create.ejs');
+});
+router.post('/new', check('title').isLength({min:1 , max:100}),
+  function(req,res, next){
+    let errs = validationResult(req);
+    console.log(errs);
+    if(errs['errors'].length > 0){
+      res.render('Seomun_notice',{errs : errs['errors']});
+    }else{
+      let param = JSON.parse(JSON.stringify(req.body));
+      let title = param['title'];
+      let description = param['content'];
+      db.create(title, description, function(){
+        res.redirect('/notice');
+      });
+    }
+  });
+
 //공지사항 list description page
 router.get('/notice/:no', (req,res) => {
   var no = req.params.no;
   db.page(no, (result) => {
-      res.render('service_notice_desc', { result : result });
+      res.render('service_notice_desc.ejs', { result : result });
     }
   );
 });
+//공지사항 list update page
+router.get('/update/:no', (req,res) =>{
+  var no = req.params.no;
+  db.update(no, (row) => {
+    if(typeof no === 'undefined' || row.length <= 0){
+      res.status(404).json({error : 'undefined memo'});
+    }else{
+      res.render('service_notice_udate.ejs',{row});
+    }
+  });
+});
+//공지사항 글수정 저장
+router.post('/update/:no', [check('title').isByteLength({min:1 , max:300})],(req, res) =>{
+  let errs = validationResult(req);
+  let param = JSON.parse(JSON.stringify(req.body));
+  var no = req.params.no;
+  let title = param['title'];
+  let description = param['content'];
+  if(errs['errors'].length > 0){
+    db.update_process(no, (row) => {
+      res.render('service_notice_udate.ejs', {row : row[0], errs : errs['errors']} );
+    });
+  }else{
+    db.update_process(no, title, description, () => {
+      res.redirect('/notice');
+    });
+  }
+});
+//공지사항 글삭제
+router.get('/delete/:no', (req, res) => {
+  var no = req.params.no;
+  db.remove(no, () => {
+    res.redirect('/notice');
+  });
+});
+
+//Qna page
+router.get('/qna', (req,res) => {
+  res.render('service_qna.ejs');
+});
+
+
+
 
 
 
